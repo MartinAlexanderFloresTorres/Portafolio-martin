30bf9b534623caa626b9616ee5445cbc58bc460c
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2022-02-06 23:21:30 -0500

    actualizamos el color de los iconos de contacto

--- menu/main.js
+++
@@ -1,7 +0,0 @@
-const bar = document.querySelector('.menubar');
-const item = document.querySelector('.header-item');
-
-bar.addEventListener('click', () => {
-    bar.classList.toggle('activo');
-    item.classList.toggle('activo');
-})
